60728f3516873062c50de46ceed114f47eab388d
Marcus Sauceda <marcussauceda@Marcuss-iMac.local>
2021-09-16 13:26:05 -0500

    added heroku links to fetch

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends Component {
   onButtonSubmit = () => {
     // console.log('click');
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://thawing-hamlet-94541.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -113,7 +113,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if(response){
-        fetch('http://localhost:3000/image', {
+        fetch('https://thawing-hamlet-94541.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
